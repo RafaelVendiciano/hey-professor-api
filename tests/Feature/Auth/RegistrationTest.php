c866818ffbda2187465b7274bd57ba528ae73e62
Rafael Barbosa <rafaelvbarbosa3@gmail.com>
2024-04-20 10:00:53 -0300

    removed describe in RegistrationTest

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -28,22 +28,5 @@
         );
     });
 
-    describe('validations', function () {
-
-        test('name', function ($rule, $value, $meta = []) {
-            postJson(route('register'), ['name' => $value])
-                ->assertJsonValidationErrors([
-                    'name' => __(
-                        'validation.' . $rule,
-                        array_merge(['attribute' => 'name'], $meta)
-                    ),
-                ]);
-        })->with([
-            'required' => ['required', ''],
-            'min:3'    => ['min', 'AB', ['min' => 3]],
-            'max:255'  => ['max', str_repeat('*', 256), ['max' => 255]],
-        ]);
-    
-    });
 
 ?>
